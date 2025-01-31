9cf46d9378af453990da5fb9ff9349ffd2340cfe
Johan Hermansson <johan@sumway.se>
2025-01-31 15:37:26 +0100

    🔍 Updated GoogleService to handle multiple account ids 🔍 Added getInvitations and accectInvitation methods to GoogleService

--- app/Services/GoogleService.php
+++ app/Services/GoogleService.php
@@ -32,37 +32,67 @@ class GoogleService {
 		$this->accessToken = $this->client->fetchAccessTokenWithAssertion()['access_token'];
 	}
 
-	public function getAccounts() {
-		$response = Http::withToken($this->accessToken)
-			->get("https://mybusinessaccountmanagement.googleapis.com/v1/accounts");
+	public function getInvitations(string|int $accountId) {
+		$invitations = Cache::remember("google/invitations", 10, function () use ($accountId) {
+			$response = Http::withToken($this->accessToken)
+				->get("https://mybusinessaccountmanagement.googleapis.com/v1/accounts/{$accountId}/invitations");
 
-		return $response->json('accounts');
+			return $response->json('invitations');
+		});
+
+		return $invitations;
 	}
 
-	public function getAccountId($accountName = null) {
+	public function acceptInvitation(string|int $accountId, string|int $invitationId): bool {
+		// Accept invitation with empty body
+		$response = Http::withToken($this->accessToken)
+			->post("https://mybusinessaccountmanagement.googleapis.com/v1/accounts/{$accountId}/invitations/{$invitationId}:accept", null);
+
+		return $response->successful();
+	}
+
+	public function getAccounts(bool $forceUpdate = false) {
+		if ($forceUpdate) {
+			Cache::forget('google/accounts');
+		}
+
+		$accounts = Cache::remember("google/accounts", 300, function () {
+			$response = Http::withToken($this->accessToken)
+				->get("https://mybusinessaccountmanagement.googleapis.com/v1/accounts");
+
+			return $response->json('accounts');
+		});
+
+		return $accounts;
+	}
+
+	public function getAccountIds() {
 		if (getenv('GOOGLE_BUSINESS_ACCOUNT_ID')) {
-			return getenv('GOOGLE_BUSINESS_ACCOUNT_ID');
+			$accountIds = explode(',', getenv('GOOGLE_BUSINESS_ACCOUNT_ID'));
+			return array_map('trim', $accountIds);
 		}
 
-		$account = collect($this->getAccounts())->firstWhere('accountName', $accountName);
-
-		$accountId = collect($account)->get('name');
-
-		if ($accountId) {
-			return explode('/', $accountId)[1] ?? null;
-		}
-
-		return null;
+		return collect($this->getAccounts())
+			->filter(function ($account) {
+				// Only Memira accounts and not NL accounts
+				return preg_match('/Memira/', $account['accountName']) and !preg_match('/NL/', $account['accountName']);
+			})
+			->pluck('name')
+			->map(function ($name) {
+				$name = explode('/', $name);
+				return end($name);
+			})
+			->all();
 	}
 
 	public function getLocations(bool $forceUpdate = false) {
-		$accountId = $this->getAccountId();
+		$accountIds = $this->getAccountIds();
 
 		if ($forceUpdate) {
 			Cache::forget('google/locations');
 		}
 
-		$locations = Cache::remember("google/locations", 300, function () use ($accountId) {
+		$locations = Cache::remember("google/locations", 300, function () use ($accountIds) {
 			$readMask = [
 				'name',
 				'title',
@@ -79,19 +109,21 @@ class GoogleService {
 			$allLocations = [];
 			$nextPageToken = null;
 
-			do {
-				$response = Http::withToken($this->accessToken)
-					->get("https://mybusiness.googleapis.com/v1/accounts/{$accountId}/locations", [
-						'pageSize' => 100,
-						'readMask' => implode(',', $readMask),
-						'pageToken' => $nextPageToken,
-					]);
+			foreach ($accountIds as $accountId) {
+				do {
+					$response = Http::withToken($this->accessToken)
+						->get("https://mybusiness.googleapis.com/v1/accounts/{$accountId}/locations", [
+							'pageSize' => 100,
+							'readMask' => implode(',', $readMask),
+							'pageToken' => $nextPageToken,
+						]);
 
-				$responseJson  = $response->json();
-				$locations     = $responseJson['locations'] ?? [];
-				$allLocations  = array_merge($allLocations, $locations);
-				$nextPageToken = $responseJson['nextPageToken'] ?? null;
-			} while ($nextPageToken);
+					$responseJson  = $response->json();
+					$locations     = $responseJson['locations'] ?? [];
+					$allLocations  = array_merge($allLocations, $locations);
+					$nextPageToken = $responseJson['nextPageToken'] ?? null;
+				} while ($nextPageToken);
+			}
 
 			return $allLocations;
 		});
